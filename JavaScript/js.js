0eb3c259949681314a968db703656b2284f57067
Mayank kumar <mayank.swarnkarr@gmail.com>
2025-07-07 13:56:25 +0530

    day 20

--- JavaScript/js.js
+++ JavaScript/js.js
@@ -1,1 +1,17 @@
 console.log('Jai Shree Ram');
+
+function login() {
+    console.log('Har Har Mahadev')
+}
+
+login();
+
+let a = 10;
+let b = 20;
+console.log('sum of a and b is : ' + (a + b))
+
+const abc = [1,3,6,7,2,0];
+
+    abc.push();
+    console.log(abc.push());
+
